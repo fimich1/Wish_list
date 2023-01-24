328296ae393ae7807c5a47a4bb5c8fb93bb0604d
fimich1 <38435351+fimich1@users.noreply.github.com>
2023-01-25 00:15:34 +0500

    начало

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -7,40 +7,58 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-                radius: 30.0,
-                backgroundColor: Colors.white,
-                child: Icon(
-                  Icons.list,
-                  size: 30.0,
-                  color: Colors.lightBlueAccent,
-                )),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Метнись-ка:',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              '12 раз',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-            Container(
-              color: ,
-            )
-          ],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(
+          Icons.add,
         ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                    radius: 30.0,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.list,
+                      size: 30.0,
+                      color: Colors.lightBlueAccent,
+                    )),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  'Метнись-ка:',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Text(
+                  '12 раз',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                  )),
+            ),
+          )
+        ],
       ),
     );
   }
