65a4e9118235d6fed5616723ade2169cbd65b813
fimich1 <38435351+fimich1@users.noreply.github.com>
2023-02-11 01:58:34 +0500

    чекбоксы

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool? isChecked = false;
+
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Напряг 2'),
-      trailing: Checkbox(value: false, onChanged: null),
+      title: Text('Напряг 2',
+        style: TextStyle(
+          decoration: isChecked == true ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked,
+          toggleCheckboxState: (checkboxState) {
+      setState(() {
+      isChecked = checkboxState;
+      });
+
+      }
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool? checkboxState;
+  final Function(bool?)? toggleCheckboxState;
+
+  TaskCheckbox(
+  {@required this.checkboxState, @required this.toggleCheckboxState}
+      );
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: checkboxState,
+        onChanged: toggleCheckboxState,
     );
   }
 }
